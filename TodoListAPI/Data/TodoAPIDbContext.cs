6c927e442ff1b1bf10241df2c79dc71a354f064c
owen1077 <osasumwen.odigie@decagon.dev>
2022-11-10 16:19:00 +0100

    first commit

---
+++ TodoListAPI/Data/TodoAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListAPI.Models;
+
+namespace TodoListAPI.Data
+{
+    public class TodoAPIDbContext : DbContext
+    {
+        public TodoAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
